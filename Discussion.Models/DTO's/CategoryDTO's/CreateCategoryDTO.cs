4b8a8b841afd41d4954d2a39d002c84e1247b044
Mateusz Cogiel <jank7947@gmail.com>
2023-08-30 19:11:57 +0200

    Category Service

---
+++ Discussion.Models/DTO's/CategoryDTO's/CreateCategoryDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Discussion.Models.DTO_s.CategoryDTO;
+
+/// <summary>
+/// A Data Transfer Object for creation of Category type.
+/// </summary>
+public class CreateCategoryDTO
+{
+    /// <summary>
+    /// Name of Category that will be created.
+    /// </summary>
+    public string Name { get; set; }
+}
---
+++ Discussion.Models/DTO's/QuestionDTO's/QuestionDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Discussion.Models.DTO_s.QuestionDTO_s;
+
+public class QuestionDTO
+{
+    public static QuestionDTO ToQuestionDTO(object obj)
+    {
+        Type type = obj.GetType();
+        var properties = type.GetProperties();
+
+        return new QuestionDTO
+        {
+
+        };
+    }
+}
