d1dd8c49698076d5414f2b35ade61db6eb799948
Mateusz Cogiel <jank7947@gmail.com>
2023-09-20 11:28:18 +0200

    Add CORS Configuration.

---
+++ Discussion.WebAPI/ProgramExtensions/CorsSetup.cs
@@ -0,0 +1,21 @@
+﻿namespace Discussion.WebAPI.ProgramExtensions;
+
+/// <summary>
+/// CORS Configuration.
+/// </summary>
+public static class CorsSetup
+{
+    public static IServiceCollection RegisterCors(this IServiceCollection services, WebApplicationBuilder _builder)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("FrontEndClient", builder =>
+            builder.AllowAnyMethod() 
+            .AllowAnyHeader()
+            .WithOrigins(_builder.Configuration["AllowedOrigins"])); 
+        });
+
+        return services;
+    }
+}
+
